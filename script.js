0ca4e6a1514f849d13f6f10bf673eca3f92d7435
musfikur-rahman <musfik.rizvi@gmail.com>
2022-08-23 17:14:53 +0600

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ async function RestSingleCountriesByFetch() {
                 document.getElementById('regionalBlocs').value = ''; 
             }
             document.getElementById('cioc').value = obj.cioc;
-            document.getElementById('flag').src = obj.flag[0]; 
+            document.getElementById('flag').src = obj.flag[0].svg; 
             document.getElementById('flag').className = 'w3-image w3-border w3-border-black';
         }
         catch (error) {
